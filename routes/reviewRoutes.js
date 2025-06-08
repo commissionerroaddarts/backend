868e588a18d9d371f78ed820f4bb8d1ebfa41f10
Commissioner <commissioner@roaddarts.com>
2025-06-08 18:51:59 +0500

    removed auth middleware from get all reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,15 +1,24 @@
-import express from 'express';
-import { authMiddleware } from './../middlewares/authMiddleware.js';
-import { createReview, getAllReviews, deleteReview, bulkCreateReviews, updateReview } from '../controllers/reviewsController.js';
-import { validateCreateReview, validateUpdateReview } from '../validation/reviewValidation.js';
+import express from "express";
+import { authMiddleware } from "./../middlewares/authMiddleware.js";
+import {
+  createReview,
+  getAllReviews,
+  deleteReview,
+  bulkCreateReviews,
+  updateReview,
+} from "../controllers/reviewsController.js";
+import {
+  validateCreateReview,
+  validateUpdateReview,
+} from "../validation/reviewValidation.js";
 
 const router = express.Router();
 
-router.post('/', authMiddleware, validateCreateReview, createReview);
-router.post('/bulk', bulkCreateReviews);
+router.post("/", authMiddleware, validateCreateReview, createReview);
+router.post("/bulk", bulkCreateReviews);
 // router.get('/:business', authMiddleware, getAllReviews);
-router.get('/', authMiddleware, getAllReviews);
-router.delete('/:id', authMiddleware, deleteReview);
-router.patch('/:id', authMiddleware, validateUpdateReview, updateReview);
+router.get("/", getAllReviews);
+router.delete("/:id", authMiddleware, deleteReview);
+router.patch("/:id", authMiddleware, validateUpdateReview, updateReview);
 
 export default router;
