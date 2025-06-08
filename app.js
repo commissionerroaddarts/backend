af21d48f3e74dd96fbedbac8233bd8d1b060bf2a
Commissioner <commissioner@roaddarts.com>
2025-06-08 17:13:42 +0500

    added searching by address and user location,radius

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-import businessRoutes from './routes/businessRoutes.js';
-import promotionRoutes from './routes/promotionRoutes.js';
-import eventRoutes from './routes/eventRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import authRoutes from './routes/authRoutes.js';
-import contactRoutes from './routes/contactRoutes.js';
-import reviewRoutes from './routes/reviewRoutes.js';
-import subscriptionRoutes from './routes/subscriptionRoutes.js';
-import notificationRoutes from './routes/notificationRoutes.js';
+import businessRoutes from "./routes/businessRoutes.js";
+import promotionRoutes from "./routes/promotionRoutes.js";
+import eventRoutes from "./routes/eventRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import contactRoutes from "./routes/contactRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+import subscriptionRoutes from "./routes/subscriptionRoutes.js";
+import notificationRoutes from "./routes/notificationRoutes.js";
 
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import passport from 'passport';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import passport from "passport";
 import "./config/passport.js";
-import { stripeWebhookFn } from './config/stripe.js';
-import bodyParser from 'body-parser';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { globalRateLimiter } from './middlewares/rateLimiters.js';
+import { stripeWebhookFn } from "./config/stripe.js";
+import bodyParser from "body-parser";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { globalRateLimiter } from "./middlewares/rateLimiters.js";
 
 dotenv.config();
-
 
 // express setup
 const app = express();
 
 app.use(globalRateLimiter);
 
-app.post('/webhook', bodyParser.raw({ type: 'application/json' }), stripeWebhookFn );
-
+app.post(
+  "/webhook",
+  bodyParser.raw({ type: "application/json" }),
+  stripeWebhookFn
+);
 
 // ===== MIDDLEWARES =====
 app.use(cookieParser());
@@ -37,56 +39,52 @@ app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
-    "http://localhost:3000", 
-    process.env.FRONTEND_URL,
-    process.env.FRONTEND_URL_WWW
+  "http://localhost:3000",
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_URL_WWW,
 ];
 
-
-app.use(cors({
+app.use(
+  cors({
     origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true); // Allow request
-        } else {
-            callback(new Error("Not allowed by CORS")); // Block request
-        }
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true); // Allow request
+      } else {
+        callback(new Error("Not allowed by CORS")); // Block request
+      }
     },
     credentials: true, // Allow cookies to be sent
-}));
+  })
+);
 
 // DB Connection
 connectDB();
 
-
-
 app.use(passport.initialize());
 
-
 // =========================== Routes ===========================
 
-app.get('/', (req, res) => res.json({ message: "✅ API is running... 3" }));
-app.use('/api/businesses', businessRoutes);
-app.use('/api/events', eventRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/contactus', contactRoutes);
-app.use('/api/subscription', subscriptionRoutes); 
-app.use('/api/reviews', reviewRoutes); 
-app.use('/api/promotion', promotionRoutes); 
-app.use('/api/notifications', notificationRoutes); 
-
+app.get("/", (req, res) => res.json({ message: "✅ API is running... 3" }));
+app.use("/api/businesses", businessRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/contactus", contactRoutes);
+app.use("/api/subscription", subscriptionRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/promotion", promotionRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // =========================== Routes ===========================
-
 
 app.use((req, res, next) => {
-    res.status(404).json({ message: "Route Not Found" });
+  res.status(404).json({ message: "Route Not Found" });
 });
 
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
-    console.log(`✅ Server is running on port ${process.env.PORT}`);
-})
+  console.log(`✅ Server is running on port ${process.env.PORT}`);
+});
 
 export default app;
