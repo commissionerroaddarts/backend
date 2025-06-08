223ad89748afc1c43dc73d3ea879527b5772a158
Commissioner <commissioner@roaddarts.com>
2025-06-08 20:24:48 +0500

    added searching by address and user location,radius

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   createBusiness,
   getAllBusinesses,
@@ -12,36 +12,36 @@ import {
   sendMessageToOwner,
   checkBusinessNameAvailability,
   updateSlugsForAllBusinesses,
-  getBusinessBySlug
-} from '../controllers/businessController.js';
-import upload from '../middlewares/uploadMiddleware.js';
-import { authMiddleware } from './../middlewares/authMiddleware.js';
-import { validateCreate, validateUpdate } from '../validation/businessValidation.js';
+  getBusinessBySlug,
+} from "../controllers/businessController.js";
+import upload from "../middlewares/uploadMiddleware.js";
+import { authMiddleware } from "./../middlewares/authMiddleware.js";
+import {
+  validateCreate,
+  validateUpdate,
+} from "../validation/businessValidation.js";
 
 const router = express.Router();
 
 const uploadMedia = upload.fields([
-  { name: 'images', maxCount: 10 },  // for multiple images
-  { name: 'businessLogo', maxCount: 1 },  // for a single logo
-  { name: 'businessCover', maxCount: 1 },  
-  
+  { name: "images", maxCount: 10 }, // for multiple images
+  { name: "businessLogo", maxCount: 1 }, // for a single logo
+  { name: "businessCover", maxCount: 1 },
 ]);
 
 // CRUD Routes
-router.get('/', getAllBusinesses);
-router.get('/addslug', updateSlugsForAllBusinesses);
-router.get('/:slug', getBusinessBySlug);
-router.post('/bulk', authMiddleware, bulkCreateBusinesses);
-router.patch('/media/:id', authMiddleware,uploadMedia, uploadBusinessMedia);
-router.post('/', authMiddleware, uploadMedia, validateCreate, createBusiness);
-router.patch('/:id', authMiddleware, validateUpdate, updateBusiness);
-// router.patch('/upload-logo/:id', authMiddleware, upload.single('businessLogo'), uploadBusinessLogo);
-router.patch('/upload/:id', authMiddleware, uploadMedia, uploadBusinessImage);
-router.delete('/:id', authMiddleware, deleteBusiness);
-router.delete('/media/:id', authMiddleware, deleteBusinessMedia);
+router.get("/", getAllBusinesses);
+router.get("/addslug", updateSlugsForAllBusinesses);
+router.get("/:slug", getBusinessBySlug);
+router.post("/bulk", authMiddleware, bulkCreateBusinesses);
+router.patch("/media/:id", authMiddleware, uploadMedia, uploadBusinessMedia);
+router.post("/", authMiddleware, uploadMedia, validateCreate, createBusiness);
+router.patch("/:id", authMiddleware, validateUpdate, updateBusiness);
+router.patch("/upload/:id", authMiddleware, uploadMedia, uploadBusinessImage);
+router.delete("/:id", authMiddleware, deleteBusiness);
+router.delete("/media/:id", authMiddleware, deleteBusinessMedia);
 
-router.post('/contact/:id', sendMessageToOwner);
-router.post('/check-name', checkBusinessNameAvailability);
-
+router.post("/contact/:id", sendMessageToOwner);
+router.post("/check-name", checkBusinessNameAvailability);
 
 export default router;
